033c3cc263cc391b0533aef8f70da9a362b68be4
maxsdrv <maxim_sdrv@mail.ru>
2023-01-31 16:05:54 +0300

    forward_list forever

--- temp/main.cpp
+++ temp/main.cpp
@@ -43,17 +43,58 @@ int getIndexArray(std::vector<int>& v, int target) {
 	return -1;
 }
 
+std::ostream& operator<<(std::ostream& os, const std::forward_list<int>& list) {
+	for (const auto& l : list) {
+		os << ' ' << l;
+	}
+	return os;
+}
+std::ostream& operator<<(std::ostream& os, ListNode* l) {
+	while (l != nullptr) {
+		os << l->val << "->";
+		l = l->next;
+	}
+	os << '\n';
+	return os;
+}
+
 ListNode* modern_merge_list(ListNode* a, ListNode* b) {
-	std::forward_list<int> list;
+	std::forward_list<int> list_a;
+	std::forward_list<int> list_b;
 	LOG_DURATION("modern_merge_list")
 	while (a->next) {
-		list.push_front(a->val);
+		list_a.push_front(a->val);
 		a = a->next;
 	}
-	for(const auto& l : list) {
-		std::cout << l << "->";
-	}	
+	while (b->next) {
+		list_b.push_front(b->val);
+		b = b->next;
+	}
 	std::cout << '\n';
+	list_a.sort();
+	list_b.sort();
+	std::cout << "List1: " << list_a << '\n';
+	std::cout << "List2: " << list_b << '\n';
+	list_a.splice_after(list_b.begin(), list_a);
+	std::cout << "List1: " << list_a << '\n';
+	std::cout << "List2: " << list_b << '\n';
+
+}
+using F = std::forward_list<ListNode*>;
+ListNode* merge_k(ListNode* a, ListNode* b) {
+
+}
+ListNode* modern_mergeK_lists(std::vector<ListNode*>& vec) {
+	int i {};
+	ListNode* r_list = new ListNode;	
+	std::forward_list<ListNode*> f_list;
+	std::vector<int> arr(1000000);	
+	size_t size_list = f_list.max_size();		
+	for (int i = 0; i < size_list; ++i) {
+		for (int j = 1; j < size_list; ++j) {
+			f_list.push_front(merge_k(vec[i], vec[j]));
+		}
+	}
 }
 
 int main(int argc, char* argv[]) {
@@ -65,18 +106,30 @@ int main(int argc, char* argv[]) {
 
 	auto list_a = new LinkedList;
 	auto list_b = new LinkedList;
+	auto list_c = new LinkedList;
+	auto list_d = new LinkedList;
 	LOG_DURATION("main")
-	for (int i = 0; i < 100000; ++i) {
+	for (int i = 0; i < 10; ++i) {
 		list_a->push_front(i);
 	}	
 	for (int i = 10; i < 20; ++i) {
 		list_b->push_front(i);
 	}
-	auto list1 = LinkedList::get_list(list_a);
-	auto list2 = LinkedList::get_list(list_b);
+	for (int i = 0; i < 10; ++i) {
+		list_c->push_front(i);
+	}	
+	for (int i = 10; i < 20; ++i) {
+		list_d->push_front(i);
+	}
 
-	auto list_result = modern_merge_list(list1, list2);
+	auto l1 = LinkedList::get_list(list_a);
+	auto l2 = LinkedList::get_list(list_b);
+	auto l3 = LinkedList::get_list(list_c);
+	auto l4 = LinkedList::get_list(list_d);
 
+
+	std::vector<ListNode*> vec_lists {l1, l2, l3, l4, {}, {}, {}};
+	auto result_mK = modern_mergeK_lists(vec_lists);
 
 	return 0;
 }
